ab10f6012f003164f785205d25e2eed829ede970
Miguel Camba <miguel.camba@gmail.com>
2015-07-08 22:13:54 +0100

    Benchmark actions

--- app/components/new-accordion-multi-form-item.js
+++ app/components/new-accordion-multi-form-item.js
@@ -2,4 +2,13 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   classNames: ['accordion-multi-form-item'],
+  actions: {
+    benchmark() {
+      console.time('new-actions-benchmark');
+      for (let i = 0; i < 10000; i++) {
+        this.attrs.select();
+      }
+      console.timeEnd('new-actions-benchmark');
+    }
+  }
 });
