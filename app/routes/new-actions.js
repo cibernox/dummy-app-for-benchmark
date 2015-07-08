2e952462f33c9554f7525be57f74c4ef93a5a448
Miguel Camba <miguel.camba@gmail.com>
2015-07-08 20:43:36 +0100

    Replicated behavior in new route

--- app/routes/new-actions.js
+++ app/routes/new-actions.js
@@ -2,24 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return [
-      {
-        id: 1,
-        title: 'Maths',
-        tasks: [
-          { id: 1, title: '2+2', description: 'Lorem ipsum' },
-          { id: 2, title: '3/5', description: 'Lorem ipsum' },
-          { id: 3, title: '2^32', description: 'Lorem ipsum' },
-        ]
-      },
-      {
-        id: 2,
-        title: 'History',
-        tasks: [
-          { id: 4, title: 'Waterloo battle', description: 'Lorem ipsum' },
-          { id: 5, title: 'Roman empire', description: 'Lorem ipsum' },
-        ]
-      },
-    ]
+    return this.store.find('homework', 1);
   }
 });
